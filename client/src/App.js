2d6c2a5ff788b13c2bd0d793f873833987bd400e
James Hansen <jameszenartist@gmail.com>
2022-12-16 22:31:58 -0800

    update css file

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <div className="container">
         <InputTodo />
         <ListTodos />
       </div>
-    </div>
+    </>
   );
 }
 
