54111c11b69ebc1a69933dd7e22268bdc594ca6f
James Hansen <jameszenartist@gmail.com>
2023-02-27 13:07:29 -0800

    update framer motion syntax

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,21 @@ import ListTodos from "./components/ListTodos.jsx";
 import "./App.css";
 
 function App() {
-  const [mousePos, setMousePos] = useState({});
+  const [mousePos, setMousePos] = useState({
+    x: null,
+    y: null,
+  });
 
   useEffect(() => {
+    console.log(`useEffect called`);
     const handleMouseMove = (event) => {
       setMousePos({ x: event.clientX, y: event.clientY });
     };
-
     window.addEventListener("mousemove", handleMouseMove);
-
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
-
-  const variants = {
-    default: {
-      x: mousePos.x - 80,
-      y: mousePos.y - 80,
-    },
-  };
 
   return (
     <>
@@ -37,9 +32,16 @@ function App() {
         </b>
       </div> */}
       <div className="container">
-        <motion.div className="cursor" variants={variants} animate="default">
+        <motion.div
+          className="cursor"
+          animate={{
+            x: mousePos.x - 80,
+            y: mousePos.y - 80,
+          }}
+        >
           <div className="inner-circle"></div>
         </motion.div>
+
         <InputTodo />
         <ListTodos />
       </div>
